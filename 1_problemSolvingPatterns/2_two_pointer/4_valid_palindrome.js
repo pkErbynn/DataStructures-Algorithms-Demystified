b5d290c5ef541228a7e1d25f48268a73c2d476e5
Erbynn John <john.erbynn@gmail.com>
2024-03-15 05:59:52 -0500

    palidrome clarified

--- 1_problemSolvingPatterns/2_two_pointer/4_valid_palindrome.js
+++ 1_problemSolvingPatterns/2_two_pointer/4_valid_palindrome.js
@@ -3,15 +3,6 @@ Write a function that takes a string s as input and checks whether it’s a pali
 */
 
 function isPalindrome(s) {
-    // let endIndex = s.length - 1;
-    // for(let startIndex=0; startIndex<=endIndex; startIndex++){
-    //     if(s[startIndex] != s[endIndex]){
-    //         return false;
-    //     }
-    //     endIndex--;
-    // }
-    // return true;
-
     let startIndex = 0;
     let endIndex = s.length-1;
 
@@ -21,24 +12,38 @@ function isPalindrome(s) {
         }
         endIndex--;
         startIndex++;
-    }
 
-    // while(startIndex <= endIndex){
-    //     if(s[startIndex] == s[endIndex]){
-    //         startIndex++;
-    //         endIndex--;
-    //         continue;
-    //     }
-    //     return false;
-    // }
+        //// or...
+        // if(s[startIndex] == s[endIndex]){
+        //     startIndex++;
+        //     endIndex--;
+        //     continue;
+        // }
+        // return false;
+    }
 
     return true;
 }
 
-
 console.log("Result:", isPalindrome("madam"));
 console.log("Result2:", isPalindrome("racecar"));
 console.log("Result3:", isPalindrome("javascript"));
+
+
+////// using for-loop
+function isPalindrome_forloop(s) {
+    let endIndex = s.length - 1;
+    for(let startIndex=0; startIndex<=endIndex; startIndex++){
+        if(s[startIndex] != s[endIndex]){
+            return false;
+        }
+        endIndex--;
+    }
+    return true;
+}
+console.log("Result_forloop:", isPalindrome_forloop("madam"));
+console.log("Result2_forloop:", isPalindrome_forloop("racecar"));
+console.log("Result3_forloop:", isPalindrome_forloop("javascript"));
 
 
 // A palindrome is a word, phrase, number, or other sequence of characters 
