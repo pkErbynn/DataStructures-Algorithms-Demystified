461f6956e3e78a741424a921d7b3dfb115489237
Erbynn (Erb) John <john.erbynn@gmail.com>
2025-04-07 13:54:49 -0500

    Update 11_minIndexInMountainArray.js

--- 3_searching/11_minIndexInMountainArray.js
+++ 3_searching/11_minIndexInMountainArray.js
@@ -8,6 +8,7 @@ Example 1:
 Input: mountainArr = [1,2,3,4,5,3,1], target = 3
 Output: 2
 Explanation: 3 exists in the array, at index=2 and index=5. Return the minimum index, which is 2.
+
 Example 2:
 
 Input: mountainArr = [0,1,2,4,2,1], target = 3
