ccdec067f498f653f41dfbc2ca1c10333a8cb2a9
pkErbynn <john.erbynn@gmail.com>
2025-07-09 21:45:14 -0500

    add question to infinite bs

--- 3_searching/9_positionOfElementInInfiniteSortedArray.js
+++ 3_searching/9_positionOfElementInInfiniteSortedArray.js
@@ -1,5 +1,22 @@
-// Question: https://www.geeksforgeeks.org/find-position-element-sorted-array-infinite-numbers/
-// Explaination: https://youtu.be/W9QJ8HaRvJQ?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ&t=5553
+/* 
+
+Question: https://www.geeksforgeeks.org/find-position-element-sorted-array-infinite-numbers/
+
+Given a sorted array arr[] of infinite numbers. The task is to search for an element k in the array.
+
+Examples:
+
+Input: arr[] = [3, 5, 7, 9, 10, 90, 100, 130, 140, 160, 170], k = 10
+Output: 4
+Explanation: 10 is at index 4 in array.
+
+Input: arr[] = [2, 5, 7, 9], k = 3
+Output: -1
+Explanation: 3 is not present in array.
+
+Explaination: https://youtu.be/W9QJ8HaRvJQ?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ&t=5553
+
+*/
 
 findThePostitionOfTarget  = function(arr, target) {
 
