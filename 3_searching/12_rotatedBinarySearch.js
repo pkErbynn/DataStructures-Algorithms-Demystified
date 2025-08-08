1d97da3c52c073e479a8bc8ae4b9cecd7c8f5fbd
pkErbynn <john.erbynn@gmail.com>
2025-08-08 04:35:49 -0500

    rotated binSearch

--- 3_searching/12_rotatedBinarySearch.js
+++ 3_searching/12_rotatedBinarySearch.js
@@ -115,7 +115,9 @@ console.log("findTargetInRotatedArray4:", findTargetInRotatedArray([4, 5, 6, 7, 
 
 
 
-// Alternative solution, shorter and more logical **
+// Alternative solution, shorter and more logical *****
+// 1. Find the sorted part. Since it's roated, one is guaranteed to be sorted 
+// 2. Once sorted part is determined, apply binary search halfing: either within range, or outside range
 // vid: https://www.youtube.com/watch?v=5qGrJbHhqFs
 function searchInRotatedArray(nums, target) {
     let start = 0;
