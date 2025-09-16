74acf8f71acc11d5e7e914ff4cb10dd0343d49da
pkErbynn <john.erbynn@gmail.com>
2025-09-16 06:12:26 -0500

    add sqrt w/ binary search brute-force method

--- 3_searching/15_squareRootUsingBinarySearch.js
+++ 3_searching/15_squareRootUsingBinarySearch.js
@@ -22,17 +22,40 @@ Explanation: The square root of 8 is 2.82842..., and since we round it down to t
 
 
 /*
+=== Brute force: from basic intuition
 
 Sqrt(4) = 2 => 2 * 2 = 25
 Sqrt(9) = 3 => 3 * 3 = 9
-Sqrt(25) = 5 => 5 * 5 = 25
+Sqrt(28) = 5.29 = 5 floored => 5 * 5 = 25 <= 28
+    1 X 1 = 1
+    2 X 2 = 4
+    3 X 3 = 9
+    4 X 4 = 16
+    5 X 5 = 25
+    6 X 6 = 36 => No. Since 36 > 28, then 6 can't be a possible ans
 
-Brute force:
 - Iterate from 1 to the num(eg. 25), multiply current iterator num to itself 
 - and if result equal num(eg. 25), then current iterator is the answer
+- and if result is greater than the num(eg. 25) then return previous iterator value as answer
 - Time Complexity = O(n)
 
 */
-function squareRoot(number) {
-    
+function squareRoot_bruteForce(number) {
+    let possibleAns = 1; 
+
+    for(let i = 1; i <= number; i++){
+        let multiplied = i * i;
+
+        if(multiplied <= number){
+            possibleAns = i;    // posible ans while within
+        }
+        else{
+            break;     // returns prev if greater
+        }
+    }
+
+    return possibleAns;
 }
+
+console.log("squareRoot_bruteForce:", squareRoot_bruteForce(9));
+console.log("squareRoot_bruteForce:", squareRoot_bruteForce(28));
