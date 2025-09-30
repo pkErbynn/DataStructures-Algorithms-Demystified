5943f032aa221ddf92b01f56e9965c7e24814902
pkErbynn <john.erbynn@gmail.com>
2025-09-30 02:26:21 -0500

    alt algo on roman-int convertion

--- 5_abstractDataStructures/6_hash_tables/2_romanToInteger.js
+++ 5_abstractDataStructures/6_hash_tables/2_romanToInteger.js
@@ -40,3 +40,38 @@ console.log("romanToInt2", romanToInt("IV"));      // 4
 console.log("romanToInt3", romanToInt("IX"));      // 9
 console.log("romanToInt4", romanToInt("LVIII"));   // 58
 console.log("romanToInt5", romanToInt("MCMXCIV")); // 1994
+
+
+function romanToInt_forloop(str) {
+    const romanMap = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    };
+
+    let intNumberRep = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        const current = romanMap[str[i]];
+        const next = romanMap[str[i + 1]];
+
+        if (!next || current >= next) {    // Normal case (or last character, next doesn't exists)
+            intNumberRep += current;
+        } 
+        else {                             // Subtractive case
+            intNumberRep -= current;
+        }
+    }
+
+    return intNumberRep;
+}
+
+console.log("romanToInt_forloop1", romanToInt_forloop("III"));     // 3
+console.log("romanToInt_forloop2", romanToInt_forloop("IV"));      // 4
+console.log("romanToInt_forloop3", romanToInt_forloop("IX"));      // 9
+console.log("romanToInt_forloop4", romanToInt_forloop("LVIII"));   // 58
+console.log("romanToInt_forloop5", romanToInt_forloop("MCMXCIV")); // 1994
